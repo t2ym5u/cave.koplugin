d9dce2e64e522011996d965ea681462c6ddd9995
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 14:59:33 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,7 +1,7 @@
 return {
-    ["Not solved yet."]                   = { fr = "Pas encore résolu." },
-    ["Shaded: %1"] = { fr = "Noircies : %1" },
-    ["Solved!"]                               = { fr = "Résolu !" },
-    ["Cave"]                                  = { fr = "Grotte" },
-    ["Find the cave hidden inside the grid"]  = { fr = "Trouvez la grotte cachée dans la grille" },
+    ["Not solved yet."]                   = { fr = "Pas encore résolu.", es = "Aún sin resolver.", de = "Noch nicht gelöst." },
+    ["Shaded: %1"] = { fr = "Noircies : %1", es = "Sombreadas: %1", de = "Schattiert: %1" },
+    ["Solved!"]                               = { fr = "Résolu !", es = "¡Resuelto!", de = "Gelöst!" },
+    ["Cave"]                                  = { fr = "Grotte", es = "Cueva", de = "Höhle" },
+    ["Find the cave hidden inside the grid"]  = { fr = "Trouvez la grotte cachée dans la grille", es = "Encuentra la cueva oculta en la cuadrícula", de = "Finde die versteckte Höhle im Gitter" },
 }
